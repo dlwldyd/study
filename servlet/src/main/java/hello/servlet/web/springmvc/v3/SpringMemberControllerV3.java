d7889267282fa9f0966732101b39cd2231a2a9d0
dlwldyd <dlwldyd9303@naver.com>
2022-02-24 21:52:14 +0900

    주석 추가

--- servlet/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ servlet/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+//모든 핸들러 메서드의 request mapping url 앞에 /springmvc/v3/members 가 붙는다.
 @RequestMapping("/springmvc/v3/members")
 public class SpringMemberControllerV3 {
 
@@ -29,9 +30,9 @@ public class SpringMemberControllerV3 {
         Member member = new Member(username, age);
         memberRepository.save(member);
 
-        model.addAttribute("member", member);
+        model.addAttribute("member", member); //뷰에 key:member 로 member 객체가 model 에 감싸져서 전달
 
-        return "save-result";
+        return "save-result";// resources/templates/save-result.html 반환, 없으면 resources/static/save-result.html 반환
     }
 
     //@RequestMapping(method = RequestMethod.GET)
